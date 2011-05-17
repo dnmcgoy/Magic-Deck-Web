51f71f7ae192c7c882a83febf5b36a06b7592234
Donnie McGaughey <savi@donald-mcgaugheys-macbook-pro.local>
2011-05-17 16:52:37 -0400

    Switch facebook auth over to the real app.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 require 'openid/store/filesystem'
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '159791907416288', 'bbf4dd45a43071552402049cadcfc4db'
+  provider :facebook, '102823226459404', 'bbf4dd45a43071552402049cadcfc4db'
   provider :twitter, 'MwnVURZ9zbZDuFYhPiINNw', 'ihO59H9LA1ceT2nLDC5XSnwwLFfwh9VboAGR3V8F2M'
   provider :openid, nil, :name => 'google', :identifier => 'https://www.google.com/accounts/o8/id'
 end

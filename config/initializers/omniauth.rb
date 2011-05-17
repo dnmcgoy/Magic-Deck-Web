4a1c5eb539686b4cb12fa56717d7b8ed405f7a72
Donnie McGaughey <savi@donald-mcgaugheys-macbook-pro.local>
2011-05-17 16:55:26 -0400

    Copy and paste screw up.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 require 'openid/store/filesystem'
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '102823226459404', 'bbf4dd45a43071552402049cadcfc4db'
+  provider :facebook, '102823226459404', '4a035f8bd7ec725b030ff9588b87bf28'
   provider :twitter, 'MwnVURZ9zbZDuFYhPiINNw', 'ihO59H9LA1ceT2nLDC5XSnwwLFfwh9VboAGR3V8F2M'
   provider :openid, nil, :name => 'google', :identifier => 'https://www.google.com/accounts/o8/id'
 end
